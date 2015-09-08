5eb62fa93c642b3644c418870416383c038a4218
Kara9125 <K.nicholas9125@gmail.com>
2015-09-08 10:56:57 -0700

    basic review form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'reviews/index'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/show'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destory'
-
-  get 'reviews/index'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/show'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destory'
-
   # user routes
   get "/signup", to: "users#new" 
   get "/profile", to: "users#show"
